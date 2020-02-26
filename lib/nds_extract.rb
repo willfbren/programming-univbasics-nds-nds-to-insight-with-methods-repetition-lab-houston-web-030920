b834d8108ae22e0a848bd069a14cdd7e7e0627bb
Will Brennan <willfbren@gmail.com>
2020-02-26 19:53:54 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -26,6 +26,14 @@ end
 
 def list_of_directors(source)
   # Write this implementation
+  director_index = 0 
+  director_list = []
+  
+  while director_index < source.size do
+    director_list.push(source[director_index][:name])
+    director_index += 1 
+  end
+  director_list
 end
 
 def total_gross(source)
@@ -38,6 +46,14 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  total_gross_of_earnings = 0 
+  director_index = 0 
+  
+  while director_index < source.size do 
+    total_gross_of_earnings += gross_for_director(source[director_index])
+    director_index += 1 
+  end 
+  total_gross_of_earnings
 end
 
 
